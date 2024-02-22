30d8eaa279009da9451f6690c23166298ee0c403
ryan2625 <ryanfreas19@augustana.edu>
2024-02-22 03:47:13 -0600

    React with TypeScript

---
+++ typescript-pratice/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+
+  let name: string = "Ryan"
+  let age: number = 22
+  let isEmployed: boolean = true
+
+  return (
+    <div className="App">
+      Hey worlsd
+    </div>
+  );
+}
+
+export default App;
