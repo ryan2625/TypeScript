f740b477d0f8ca9f25a15d12bd18ab77b9032c9e
ryan2625 <ryanfreas19@augustana.edu>
2024-02-23 00:22:22 -0600

    Testing interface, React.FormEvent, etc...

--- typescript-pratice/src/App.tsx
+++ typescript-pratice/src/App.tsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useState }  from 'react'
+import InputF from "./components/inputField"
+import { Todo } from "./model"
 import './App.css';
 
-function App() {
+const App: React.FC = () =>  {
+  const [todo, setTodo] = useState<string>("")
+  const [todos, setTodos] = useState<Todo[]>([])
 
-  let name: string = "Ryan"
-  let age: number = 22
-  let isEmployed: boolean = true
+  console.log(todo)
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault()
+    setTodos([...todos, {id: Date.now(), todo: todo, isDone:false}])
+  }
 
   return (
     <div className="App">
-      Hey worlsd
+      <span className="head">Taskify</span>
+      <InputF todo={todo} setTodo={setTodo} handleAdd={handleAdd}  /> 
+      {
+        todos.map((item, index) => (
+          <li key={index}>{item.todo}</li>
+        ))
+      }
     </div>
   );
 }
